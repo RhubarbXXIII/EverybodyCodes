2f833c5c6d3a2ed6b26aeda46e55cf70ad46abd7
Zach <zachary.philip.wilson@gmail.com>
2025-11-21 12:51:37 -0500

    quest 11

---
+++ 2025/quest11/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"flag"
+	"strconv"
+	"strings"
+
+	"everybodycodes/utils"
+)
+
+func updatePhase1(columns []int) bool {
+	updated := false
+
+	for i := range len(columns) - 1 {
+		if columns[i] > columns[i+1] {
+			columns[i]--
+			columns[i+1]++
+
+			updated = true
+		}
+	}
+
+	return updated
+}
+
+func updatePhase2(columns []int) bool {
+	updated := false
+
+	for i := range len(columns) - 1 {
+		if columns[i] < columns[i+1] {
+			columns[i]++
+			columns[i+1]--
+
+			updated = true
+		}
+	}
+
+	return updated
+}
+
+func solvePart1(input string) string {
+	columns, _ := utils.ParseInts(strings.Split(input, "\n"))
+
+	roundCount := 10
+	phaseIndex := 1
+	for range roundCount {
+		if phaseIndex == 1 {
+			if updated := updatePhase1(columns); !updated {
+				phaseIndex++
+			}
+		}
+
+		if phaseIndex == 2 {
+			if updated := updatePhase2(columns); !updated {
+				break
+			}
+		}
+	}
+
+	checksum := 0
+	for i, column := range columns {
+		checksum += (i + 1) * column
+	}
+
+	return strconv.Itoa(checksum)
+}
+
+func solvePart2(input string) string {
+	columns, _ := utils.ParseInts(strings.Split(input, "\n"))
+
+	roundCount := 0
+
+	phaseIndex := 1
+	for ; ; roundCount++ {
+		if phaseIndex == 1 {
+			if updated := updatePhase1(columns); !updated {
+				phaseIndex++
+			}
+		}
+
+		if phaseIndex == 2 {
+			if updated := updatePhase2(columns); !updated {
+				break
+			}
+		}
+	}
+
+	return strconv.Itoa(roundCount)
+}
+
+func solvePart3(input string) string {
+	columns, _ := utils.ParseInts(strings.Split(input, "\n"))
+
+	roundCount := 0
+	// for {
+	// 	equal := true
+	// 	for i := range len(columns) {
+	// 		if columns[0] != columns[i] {
+	// 			equal = false
+	// 		}
+	// 	}
+
+	// 	if equal {
+	// 		break
+	// 	}
+
+	// 	minimumDifference := math.MaxInt64
+
+	// 	for i := range len(columns) - 1 {
+	// 		difference := columns[i+1] - columns[i]
+	// 		if difference > 0 {
+	// 			minimumDifference = min(minimumDifference, columns[i+1]-columns[i])
+	// 		}
+	// 	}
+
+	// 	for i := range len(columns) - 1 {
+	// 		columns[i] += minimumDifference
+	// 		columns[i+1] -= minimumDifference
+	// 	}
+
+	// 	roundCount += minimumDifference
+	// }
+
+	birdCount := 0
+	for _, birdColumnCount := range columns {
+		birdCount += birdColumnCount
+	}
+
+	finalBirdColumnCount := birdCount / len(columns)
+
+	for _, birdColumnCount := range columns {
+		roundCount += max(birdColumnCount-finalBirdColumnCount, 0)
+	}
+
+	return strconv.Itoa(roundCount)
+}
+
+func main() {
+	submit := flag.Bool("submit", false, "Submit answers instead of dry run")
+	flag.Parse()
+
+	utils.Run(solvePart1, solvePart2, solvePart3, *submit)
+}
